1570da4c0ba82224ee43210f7ab44f33b03ba3d7
Nikunjkumar Gohel <nikunj.gohel1988@gmail.com>
2019-10-31 00:01:50 +0530

    Test Cases Green (After writing initial algorithm for number addition)

--- src/main/java/demo/tdd/string/calculator/StringCalculator.java
+++ src/main/java/demo/tdd/string/calculator/StringCalculator.java
@@ -3,7 +3,18 @@ package demo.tdd.string.calculator;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		return -1;
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		String[] numberArray = numbers.split(",");
+
+		int sum = 0;
+
+		for (String number : numberArray) {
+			sum = sum + Integer.parseInt(number);
+		}
+
+		return sum;
 	}
 
 }
