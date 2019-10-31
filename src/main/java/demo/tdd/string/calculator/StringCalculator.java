ad5fd77712b2fd6680c3da9bed06cc64e0383a87
Nikunjkumar Gohel <nikunj.gohel1988@gmail.com>
2019-10-31 09:08:17 +0530

    Re-factored code to support modular approach - Extracted constants and private utility methods out of "add" method

--- src/main/java/demo/tdd/string/calculator/StringCalculator.java
+++ src/main/java/demo/tdd/string/calculator/StringCalculator.java
@@ -2,30 +2,43 @@ package demo.tdd.string.calculator;
 
 public class StringCalculator {
 
-	private static String NUM_SEPARATOR = "[\\n,]";
+	private static final String DEFAULT_NUM_SEPARATORS = "[\\n|,]";
+	private static final int DEFAULT_RETURN_VALUE = 0;
+
+	private static final String CUSTOM_SEPARATORS_START_PATTERN = "//";
+	private static final String CUSTOM_SEPARATORS_END_PATTERN = "\n";
+
+	private static final String SPACE = " ";
+
+	private String[] numberArray;
+	private StringBuilder numSeperators;
 
 	public int add(String numbers) throws Exception {
 
 		boolean negNumbersPresent = false;
-		StringBuilder sb = null;
+		StringBuilder exceptionMessage = null;
 
 		if (numbers.isEmpty()) {
-			return 0;
+			return DEFAULT_RETURN_VALUE;
 		}
 
-		String[] numberArray;
+		if (numbers.startsWith(CUSTOM_SEPARATORS_START_PATTERN)) {
 
-		if (numbers.startsWith("//")) {
-			int startIndex = numbers.indexOf("//") + 2;
-			int newLineIndex = numbers.indexOf("\n", startIndex);
-			String separators = numbers.substring(startIndex, newLineIndex);
+			if (numSeperators == null) {
+				numSeperators = new StringBuilder();
+			}
+
+			int startIndex = numbers.indexOf(CUSTOM_SEPARATORS_START_PATTERN) + 2;
+			int newLineIndex = numbers.indexOf(CUSTOM_SEPARATORS_END_PATTERN, startIndex);
+
+			numSeperators.append(numbers.substring(startIndex, newLineIndex));
 
 			numbers = numbers.substring(newLineIndex + 1);
 
-			numberArray = numbers.split(separators);
+			numberArray = numbers.split(numSeperators.toString());
 
 		} else {
-			numberArray = numbers.split(NUM_SEPARATOR);
+			numberArray = numbers.split(DEFAULT_NUM_SEPARATORS);
 		}
 
 		int sum = 0;
@@ -33,29 +46,47 @@ public class StringCalculator {
 		for (String number : numberArray) {
 			int curNum = Integer.parseInt(number);
 
-			if (curNum > 1000) {
+			if (shouldIgnoreNumber(curNum)) {
 				continue;
 			}
 
-			if (curNum < 0) {
+			if (isNegativeNumber(curNum)) {
 				negNumbersPresent = true;
-				if (sb == null) {
-					sb = new StringBuilder("negatives not allowed:");
-				}
-				sb.append(" " + curNum);
-			}
-
-			if (negNumbersPresent)
+				exceptionMessage = buildNegativesNotAllowedExceptionMessage(exceptionMessage, curNum);
 				continue;
+			}
 
 			sum = sum + curNum;
 		}
 
 		if (negNumbersPresent) {
-			throw new Exception(sb.toString());
+			throw new Exception(exceptionMessage.toString());
 		}
 
 		return sum;
+	}
+
+	private boolean shouldIgnoreNumber(int num) {
+		if (num > 1000) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isNegativeNumber(int num) {
+		if (num < 0) {
+			return true;
+		}
+		return false;
+	}
+
+	private StringBuilder buildNegativesNotAllowedExceptionMessage(StringBuilder exceptionMessage, int num) {
+		if (exceptionMessage == null) {
+			exceptionMessage = new StringBuilder("negatives not allowed:");
+		}
+		exceptionMessage.append(SPACE + num);
+
+		return exceptionMessage;
 	}
 
 }
